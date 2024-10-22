5dbdb87a3ccd30d3b31f180a3a77468c4f8300a6
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2024-10-22 07:42:38 +0300

    Refactor reposotory class to refresh the data

--- data/src/main/java/com/mahmoud/data/repository/GithubReposRepositoryImpl.kt
+++ data/src/main/java/com/mahmoud/data/repository/GithubReposRepositoryImpl.kt
@@ -2,21 +2,26 @@ package com.example.odcgithubrepoapp.data.repository
 
 import com.example.odcgithubrepoapp.data.data_sources.local.GithubLocalDataSource
 import com.example.odcgithubrepoapp.data.data_sources.remote.GithubRemoteDataSource
-import com.example.odcgithubrepoapp.data.mapper.toGithubReposDomainModel
 import com.example.odcgithubrepoapp.data.mapper.toRepoDetailsDomainModel
 import com.example.odcgithubrepoapp.domain.model.GithubReposDomainModel
 import com.example.odcgithubrepoapp.domain.model.RepoDetailsDomainModel
 import com.example.odcgithubrepoapp.domain.repository.GithubReposRepository
+import com.mahmoud.data.mapper.toGithubReposDomainModel
+import com.mahmoud.data.mapper.toRepoEntity
 import javax.inject.Inject
 
 class GithubReposRepositoryImpl @Inject constructor(
     private val githubRemoteDataSource: GithubRemoteDataSource,
     private val githubLocalDataSource: GithubLocalDataSource,
-): GithubReposRepository {
+) : GithubReposRepository {
+
+    override suspend fun refreshReposList() {
+        val remoteRepoList = githubRemoteDataSource.fetchRepositoriesList().items
+        githubLocalDataSource.insertRepos(remoteRepoList.map { it.toRepoEntity() })
+    }
+
     override suspend fun fetchReposList(): List<GithubReposDomainModel> {
-        return githubRemoteDataSource.fetchRepositoriesList().items.map {
-            it.toGithubReposDomainModel()
-        }
+        return githubLocalDataSource.getTrendingList().map { it.toGithubReposDomainModel() }
     }
 
     override suspend fun fetchRepoDetails(ownerName: String, name: String): RepoDetailsDomainModel {
--- domain/src/main/java/com/mahmoud/domain/repository/GithubReposRepository.kt
+++ domain/src/main/java/com/mahmoud/domain/repository/GithubReposRepository.kt
@@ -4,6 +4,7 @@ import com.example.odcgithubrepoapp.domain.model.GithubReposDomainModel
 import com.example.odcgithubrepoapp.domain.model.RepoDetailsDomainModel
 
 interface GithubReposRepository {
+    suspend fun refreshReposList()
     suspend fun fetchReposList(): List<GithubReposDomainModel>
     suspend fun fetchRepoDetails(ownerName: String, name: String): RepoDetailsDomainModel
 }
