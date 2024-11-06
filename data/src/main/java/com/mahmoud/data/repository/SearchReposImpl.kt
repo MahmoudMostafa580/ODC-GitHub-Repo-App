5f1ec17254e10c28dc1ea7b2135ffd44b2a9a177
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2024-11-06 14:59:52 +0200

    Refactor Repository class to handle paging result

--- data/src/main/java/com/mahmoud/data/repository/SearchReposImpl.kt
+++ data/src/main/java/com/mahmoud/data/repository/SearchReposImpl.kt
@@ -1,16 +1,27 @@
 package com.mahmoud.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.example.odcgithubrepoapp.data.data_sources.remote.GithubRemoteDataSource
-import com.example.odcgithubrepoapp.data.mapper.toGithubReposDomainModel
 import com.example.odcgithubrepoapp.domain.model.GithubReposDomainModel
+import com.mahmoud.data.data_sources.paging.SearchPagingSource
 import com.mahmoud.domain.repository.SearchReposRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SearchReposImpl @Inject constructor(
     private val githubRemoteDataSource: GithubRemoteDataSource
 ) : SearchReposRepository {
-    override suspend fun searchRepos(language: String, perPage: Int): List<GithubReposDomainModel> {
-        return githubRemoteDataSource.searchRepos(language, perPage).items.map { it.toGithubReposDomainModel() }
+    override suspend fun searchRepos(
+        language: String,
+        perPage: Int
+    ): Flow<PagingData<GithubReposDomainModel>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = {
+                SearchPagingSource(githubRemoteDataSource, language)
+            }
+        ).flow
     }
-
 }
--- domain/src/main/java/com/mahmoud/domain/repository/SearchReposRepository.kt
+++ domain/src/main/java/com/mahmoud/domain/repository/SearchReposRepository.kt
@@ -1,7 +1,9 @@
 package com.mahmoud.domain.repository
 
+import androidx.paging.PagingData
 import com.example.odcgithubrepoapp.domain.model.GithubReposDomainModel
+import kotlinx.coroutines.flow.Flow
 
 interface SearchReposRepository {
-    suspend fun searchRepos(language: String, perPage: Int): List<GithubReposDomainModel>
+    suspend fun searchRepos(language: String, perPage: Int): Flow<PagingData<GithubReposDomainModel>>
 }
