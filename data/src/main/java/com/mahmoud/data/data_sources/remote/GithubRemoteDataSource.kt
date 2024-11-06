5a1237c7dfa693070bd6104227ae4f9de6b117db
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2024-11-06 13:51:12 +0200

    Create Search repos API interface

--- data/src/main/java/com/mahmoud/data/data_sources/remote/GithubRemoteDataSource.kt
+++ data/src/main/java/com/mahmoud/data/data_sources/remote/GithubRemoteDataSource.kt
@@ -3,6 +3,7 @@ package com.example.odcgithubrepoapp.data.data_sources.remote
 import android.util.Log
 import com.example.odcgithubrepoapp.data.data_sources.remote.retrofit.api.RepoDetailsApi
 import com.example.odcgithubrepoapp.data.data_sources.remote.retrofit.api.RepoIssuesApi
+import com.example.odcgithubrepoapp.data.data_sources.remote.retrofit.api.ReposSearchApi
 import com.example.odcgithubrepoapp.data.data_sources.remote.retrofit.api.RepositoriesListApi
 import com.example.odcgithubrepoapp.data.data_sources.remote.retrofit.data_model.repo_details.RepoDetailsDataModel
 import com.example.odcgithubrepoapp.data.data_sources.remote.retrofit.data_model.repo_issues.RepoIssuesDataModel
@@ -13,7 +14,8 @@ import javax.inject.Inject
 class GithubRemoteDataSource @Inject constructor(
     private val repositoryListApi: RepositoriesListApi,
     private val repositoryDetailsApi: RepoDetailsApi,
-    private val repositoryIssuesApi: RepoIssuesApi
+    private val repositoryIssuesApi: RepoIssuesApi,
+    private val repositorySearchApi: ReposSearchApi
 ) {
 
     suspend fun fetchRepositoriesList(): GithubReposDataModel {
@@ -40,6 +42,14 @@ class GithubRemoteDataSource @Inject constructor(
             return repositoryIssuesApi.fetchRepoIssues(ownerName, repoName)
                 .body() as RepoIssuesDataModel
         } catch (e: Exception){
+            throw e.toCustomRemoteExceptionDomainModel()
+        }
+    }
+
+    suspend fun searchRepos(language: String): GithubReposDataModel{
+        try {
+            return repositorySearchApi.searchRepos(language).body() as GithubReposDataModel
+        }catch (e: Exception){
             throw e.toCustomRemoteExceptionDomainModel()
         }
     }
